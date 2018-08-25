b4dcf0cd2277ccc6d4f09a495db6a74f8304274e
GlennKimbleJr <glennkimble@yahoo.com>
2018-08-25 16:49:19 -0400

    Cleaned up controllers.

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Checklist;
-use Illuminate\Http\Request;
 use App\Http\Requests\CreateChecklistRequest;
 
 class ChecklistController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('checklists.index', [
@@ -27,12 +21,6 @@ class ChecklistController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(CreateChecklistRequest $request)
     {
         $checklist = Checklist::create($request->only('name'));
@@ -40,12 +28,6 @@ class ChecklistController extends Controller
         return redirect()->to(route('checklists.show', $checklist));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Checklist $checklist)
     {
         $checklist->loadMissing('items');
@@ -53,24 +35,11 @@ class ChecklistController extends Controller
         return view('checklists.show', compact('checklist'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(CreateChecklistRequest $request, Checklist $checklist)
     {
         $checklist->update($request->only('name'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Checklist $checklist)
     {
         $checklist->delete();
--- app/Http/Controllers/ChecklistItemsController.php
+++ app/Http/Controllers/ChecklistItemsController.php
@@ -4,17 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Checklist;
 use App\ChecklistItem;
-use Illuminate\Http\Request;
 use App\Http\Requests\AddChecklistItemRequest;
 
 class ChecklistItemsController extends Controller
 {
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(AddChecklistItemRequest $request, Checklist $checklist)
     {
         $checklist->items()->create($request->only('name'));
@@ -22,24 +15,11 @@ class ChecklistItemsController extends Controller
         return redirect()->to(route('checklists.show', $checklist));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(AddChecklistItemRequest $request, ChecklistItem $item)
     {
         $item->update($request->only('name'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(ChecklistItem $item)
     {
         $item->delete();
