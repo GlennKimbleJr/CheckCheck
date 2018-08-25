ee821e05133703703418af2394332e2258bdaf2b
GlennKimbleJr <glennkimble@yahoo.com>
2018-08-25 16:17:29 -0400

    Removed duplicate routes.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::resource('checklists', 'ChecklistController')->except('edit');
-Route::resource('checklists.items', 'ChecklistItemsController')->only(['store','update','destroy']);
+Route::resource('checklists.items', 'ChecklistItemsController')->only('store');
 Route::put('items/{item}/complete', 'ChecklistItemsController@toggleComplete')->name('checklists.items.complete.toggle');
 Route::delete('items/{item}', 'ChecklistItemsController@destroy')->name('checklists.items.destroy');
 Route::put('items/{item}', 'ChecklistItemsController@update')->name('checklists.items.update');
